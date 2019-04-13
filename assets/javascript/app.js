e8a5076b06d3de9c2573ec631cc0883fc23888b8
unknown <diesol42@gmail.com>
2019-04-13 12:40:59 -0700

    added search api call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,21 @@
 //WHAT TO TRACK
 // apiKey
+var apiKey = "8KbVOhLYhgRAMCKLxPTpkfNcq3fBPCVj";
 // apiURL
 // search term
 
 //WHAT TO DO
 // TODO: get the search term
+function getArticle (searchTerm) {
+   var query = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + apiKey; 
+    $.ajax( {
+        url: query,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
+}
+getArticle("Whatup");
 // TODO: make an api call
 // TODO: display the results
 
