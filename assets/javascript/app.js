1424dcac64116789f943926fec8e24dc1e401501
Phillip Booker <phillippbooker@gmail.com>
2019-04-13 12:44:04 -0700

    Adds apikey.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,11 @@
 // apiURL
 // search term
 
+var apiKey = "HtGSB1iCElZ1OYJrqIfP76deJuiZfld3";
+
 //WHAT TO DO
 // TODO: get the search term
 // TODO: make an api call
 // TODO: display the results
+
 
